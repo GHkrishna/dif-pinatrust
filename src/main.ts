17ad64744918b1b82f61bf706a21b8e590b07c96
Krishna Waske <krishna.waske@ayanworks.com>
2024-10-20 14:02:21 +0530

    feat:add swagger support
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import dotenv from 'dotenv';
-dotenv.config()
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Pinatrust')
+    .setDescription('The Pinatrust API description')
+    .setVersion('1.0')
+    .addTag('Pinatrust')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000);
 }
 bootstrap();
