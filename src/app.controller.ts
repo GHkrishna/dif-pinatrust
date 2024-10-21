43d32a237800bb32859ad8fbe91bb9b8fe413537
Krishna Waske <krishna.waske@ayanworks.com>
2024-10-21 07:22:30 +0530

    feat: upload file to pinata
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { FileUploadDto } from './dto/app.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
@@ -13,6 +22,17 @@ export class AppController {
   // Sign-up using credebl(get info, register and send VC on did), also return token(containing did)
   // Login: get presentation from adeya and verify against credebl
   // Post: Add file: to pinata
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'List of files',
+    type: FileUploadDto,
+  })
+  async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<object> {
+    console.debug('file controller::', file);
+    return this.appService.uploadFile(file);
+  }
   // Get: get file from pinata
 
   /**
@@ -20,5 +40,5 @@ export class AppController {
    * 1. (Put)Associate name to did(name + did)
    * 2. (Get)Did/name-find people from platform from name or DID. So that u can add them to files to be shared
    * 3. (Put) Share to-share file to the person(Using did or name)
-  */
+   */
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { PinataSDK } from 'pinata';
+import { File } from '@web-std/file';
 
 @Injectable()
 export class AppService {
+  private pinata = new PinataSDK({
+    pinataJwt: process.env.PINATA_JWT,
+    pinataGateway: process.env.PINATA_GATEWAY_DOMAIN,
+    pinataGatewayKey: process.env.PINATA_GATEWAY_KEYS,
+  });
+  constructor() {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async uploadFile(file: Express.Multer.File): Promise<object> {
+    try {
+      const fileReady = new File([file.buffer], file.originalname, {
+        type: file.mimetype,
+      });
+      const upload = await this.pinata.upload.file(fileReady);
+      console.debug('this is file1', fileReady);
+      return { status: 'file uploaded successfully', file: upload };
+    } catch (err) {
+      console.log('From AppService: Error while uploading file to pinata');
+      throw new Error('Error while uploading file to pinata');
+    }
   }
 }
